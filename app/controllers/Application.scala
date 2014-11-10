4a8b4085503065dcc90d9a3841444dc9b7346d79
Akie Mejia <locker47@gmail.com>
2014-11-10 18:00:34 +0900

    Added file serving of HTML webapp from the server, w/ link from API entry point; Moved HTML webapp files (duplicates) into public/ assets folder

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -25,7 +25,8 @@ object Application extends Controller {
         Some("List of invites"))
       .withLink("hoa:users", routes.Users.list().absoluteURL(),
         Some("List of users"))
-
+      .withLink("hoa:webapp", routes.Assets.at("index.html").absoluteURL(),
+        Some("Web application"))
     Ok(obj.asJsValue)
   }
 
