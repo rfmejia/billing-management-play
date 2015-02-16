2e84409c0633f2653f8ca7a687e184b630525eac
Akie Mejia <locker47@gmail.com>
2015-02-16 22:22:15 +0900

    Temporarily removed empty test documents

--- app/controllers/TestEnvironment.scala
+++ app/controllers/TestEnvironment.scala
@@ -32,18 +32,18 @@ class TestEnvironment(override implicit val env: RuntimeEnvironment[User])
 
     def insertDocumentData(implicit session: Session) = {
       (for (d <- documents) yield d).delete
-      Tenant.findByTradeName("Beast Burgers").map { tenant =>
-        Document.insert("Document 1", "invoice-1", tenant.id,
-          DateTime.parse("2015-01-01"), Json.obj())
-      }
+      // Tenant.findByTradeName("Beast Burgers").map { tenant =>
+      //   Document.insert("Document 1", "invoice-1", tenant.id,
+      //     DateTime.parse("2015-01-01"), Json.obj())
+      // }
       Tenant.findByTradeName("Jumpin' Juicers").map { tenant =>
         Document.insert("Document 2", "invoice-1", tenant.id,
           DateTime.parse("2015-02-01"), testDocument)
       }
-      Tenant.findByTradeName("Beast Burgers").map { tenant =>
-        Document.insert("Document 3", "statement-of-account-1", tenant.id,
-          DateTime.parse("2015-01-01"), Json.obj())
-      }
+      // Tenant.findByTradeName("Beast Burgers").map { tenant =>
+      //   Document.insert("Document 3", "statement-of-account-1", tenant.id,
+      //     DateTime.parse("2015-01-01"), Json.obj())
+      // }
     }
 
     ConnectionFactory.connect withSession { implicit session =>
