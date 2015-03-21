c18d31dc1ff313329e53f81225e6a6ee8b669388
Akie Mejia <locker47@gmail.com>
2015-03-21 21:53:17 +0900

    Added override for route service - startSignUpUrl

--- app/services/CustomRoutesService.scala
+++ app/services/CustomRoutesService.scala
@@ -4,7 +4,9 @@ import play.api.mvc.{ Call, RequestHeader }
 import securesocial.core.services.RoutesService
 
 class CustomRoutesService extends RoutesService.Default {
+  override def startSignUpUrl(implicit req: RequestHeader): String = 
+    absoluteUrl(controllers.routes.Invitation.startInvite())
+  
   override def handleSignUpUrl(mailToken: String)(implicit req: RequestHeader): String = 
     absoluteUrl(controllers.routes.Invitation.handleSignUp(mailToken))
-  
 }
