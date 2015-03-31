9d5e103dff3f8f15a6391165fc174b48f3f70c47
Akie Mejia <locker47@gmail.com>
2015-03-31 15:01:21 +0900

    Added name field to Role case class

--- app/com/nooovle/Roles.scala
+++ app/com/nooovle/Roles.scala
@@ -2,19 +2,19 @@ package com.nooovle
 
 import play.api.libs.json._
 
-case class Role(id: String) {
+case class Role(id: String, name: String) {
   override def toString = id
 }
 
 object Roles {
-  val Encoder = Role("encoder")
-  val Checker = Role("checker")
-  val Approver = Role("approver")
-  val Admin = Role("admin")
+  val Encoder = Role("encoder", "Encoder")
+  val Checker = Role("checker", "Checker")
+  val Approver = Role("approver", "Approver")
+  val Admin = Role("admin", "Administrator")
 
   val All: Set[Role] = Set(Encoder, Checker, Approver, Admin)
 
-  def fromStringSet(rs: Set[String]): Set[Role] = rs.map(Role(_))
+  def fromStringSet(rs: Set[String]): Set[Role] = rs.map(r => find(r)).flatten.toSet
 
-  def find(id: String): Option[Role] = All.find(_ == Role(id))
+  def find(id: String): Option[Role] = All.find(_.id == id)
 }
