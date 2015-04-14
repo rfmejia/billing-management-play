525cbb5f143947f3f048db6485c95831d2e5ed5b
Juan Carlos Yu <juancarlos.yu@rakuten.com>
2015-04-14 22:18:30 +0900

    Fixed pagination

--- public/app/components/reports/hoaapp.reports.service.js
+++ public/app/components/reports/hoaapp.reports.service.js
@@ -14,7 +14,6 @@ function reportsSrvc($resource, $q, hoalinks, queryKeys){
     return service;
 
     function getReport(queryParams) {
-        console.log(queryParams);
         var url = hoalinks.getApiLink();
         url += "reports";
         var resource = $resource(url, {}, {
