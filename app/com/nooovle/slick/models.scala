532651dab6c4fb3501ec6e708c41eb7c93ae100b
Akie Mejia <locker47@gmail.com>
2015-07-09 09:59:59 +0900

    Hardcoded String data types into Postgres 'text' type

--- app/com/nooovle/slick/models.scala
+++ app/com/nooovle/slick/models.scala
@@ -67,23 +67,23 @@ object models {
 }
 
 class SettingsModel(tag: Tag) extends Table[(String, String)](tag, "SETTINGS") {
-  def key = column[String]("KEY", O.PrimaryKey)
-  def value = column[String]("VALUE", O.NotNull)
+  def key = column[String]("KEY", O.PrimaryKey, O.DBType("TEXT"))
+  def value = column[String]("VALUE", O.NotNull, O.DBType("TEXT"))
 
   def * = (key, value)
 }
 
 class ModelInfosModel(tag: Tag) extends Table[ModelInfo](tag, "MODEL_INFOS") {
-  def modelName = column[String]("MODEL_NAME", O.NotNull)
-  def fieldName = column[String]("FIELD_NAME", O.NotNull)
-  def datatype = column[String]("DATATYPE", O.NotNull)
+  def modelName = column[String]("MODEL_NAME", O.NotNull, O.DBType("TEXT"))
+  def fieldName = column[String]("FIELD_NAME", O.NotNull, O.DBType("TEXT"))
+  def datatype = column[String]("DATATYPE", O.NotNull, O.DBType("TEXT"))
   def createForm = column[Boolean]("CREATE_FORM", O.NotNull)
   def createRequired = column[Boolean]("CREATE_REQUIRED", O.NotNull)
   def editForm = column[Boolean]("EDIT_FORM", O.NotNull)
   def editRequired = column[Boolean]("EDIT_REQUIRED", O.NotNull)
 
-  def prompt = column[Option[String]]("PROMPT")
-  def tooltip = column[Option[String]]("TOOLTIP")
+  def prompt = column[Option[String]]("PROMPT", O.DBType("TEXT"))
+  def tooltip = column[Option[String]]("TOOLTIP", O.DBType("TEXT"))
 
   def pk = primaryKey("MODEL_INFOS_PK", (modelName, fieldName))
 
@@ -91,14 +91,14 @@ class ModelInfosModel(tag: Tag) extends Table[ModelInfo](tag, "MODEL_INFOS") {
 }
 
 class UsersModel(tag: Tag) extends Table[User](tag, "USERS") {
-  def userId = column[String]("USER_ID", O.PrimaryKey)
-  def providerId = column[String]("PROVIDER_ID", O.NotNull)
-  def firstName = column[Option[String]]("FIRST_NAME")
-  def lastName = column[Option[String]]("LAST_NAME")
-  def email = column[Option[String]]("EMAIL")
-  def hasher = column[String]("HASHER", O.NotNull)
-  def password = column[String]("PASSWORD", O.NotNull)
-  def salt = column[Option[String]]("SALT")
+  def userId = column[String]("USER_ID", O.PrimaryKey, O.DBType("TEXT"))
+  def providerId = column[String]("PROVIDER_ID", O.NotNull, O.DBType("TEXT"))
+  def firstName = column[Option[String]]("FIRST_NAME", O.DBType("TEXT"))
+  def lastName = column[Option[String]]("LAST_NAME", O.DBType("TEXT"))
+  def email = column[Option[String]]("EMAIL", O.DBType("TEXT"))
+  def hasher = column[String]("HASHER", O.NotNull, O.DBType("TEXT"))
+  def password = column[String]("PASSWORD", O.NotNull, O.DBType("TEXT"))
+  def salt = column[Option[String]]("SALT", O.DBType("TEXT"))
 
   def * = (providerId, userId, firstName, lastName, email, hasher, password, salt) <>
     (User.tupled, User.unapply)
@@ -106,19 +106,19 @@ class UsersModel(tag: Tag) extends Table[User](tag, "USERS") {
 
 class TenantsModel(tag: Tag) extends Table[Tenant](tag, "TENANTS") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-  def tradeName = column[String]("TRADE_NAME", O.NotNull)
-  def address = column[String]("ADDRESS", O.NotNull)
-  def contactPerson = column[String]("CONTACT_PERSON", O.NotNull)
-  def contactNumber = column[String]("CONTACT_NUMBER", O.NotNull)
-  def email = column[String]("EMAIL", O.NotNull)
+  def tradeName = column[String]("TRADE_NAME", O.NotNull, O.DBType("TEXT"))
+  def address = column[String]("ADDRESS", O.NotNull, O.DBType("TEXT"))
+  def contactPerson = column[String]("CONTACT_PERSON", O.NotNull, O.DBType("TEXT"))
+  def contactNumber = column[String]("CONTACT_NUMBER", O.NotNull, O.DBType("TEXT"))
+  def email = column[String]("EMAIL", O.NotNull, O.DBType("TEXT"))
 
   def area = column[Double]("AREA", O.NotNull)
-  def rentalPeriod = column[String]("RENTAL_PERIOD", O.NotNull)
+  def rentalPeriod = column[String]("RENTAL_PERIOD", O.NotNull, O.DBType("TEXT"))
   def escalation = column[Double]("ESCALATION", O.NotNull)
 
   def cusaDefault = column[Option[Double]]("CUSA_DEFAULT")
-  def waterMeterDefault = column[Option[String]]("WATER_METER_DEFAULT")
-  def electricityMeterDefault = column[Option[String]]("ELECTRICITY_METER_DEFAULT")
+  def waterMeterDefault = column[Option[String]]("WATER_METER_DEFAULT", O.DBType("TEXT"))
+  def electricityMeterDefault = column[Option[String]]("ELECTRICITY_METER_DEFAULT", O.DBType("TEXT"))
   def baseRentDefault = column[Option[Double]]("BASE_RENT_DEFAULT")
   def standardMultiplierDefault = column[Option[Double]]("STANDARD_MULTIPLIER_DEFAULT")
 
@@ -127,8 +127,8 @@ class TenantsModel(tag: Tag) extends Table[Tenant](tag, "TENANTS") {
 }
 
 class UserRolesModel(tag: Tag) extends Table[(String, String)](tag, "USER_ROLES") {
-  def userId = column[String]("USERNAME", O.NotNull)
-  def roleName = column[String]("ROLE_NAME", O.NotNull)
+  def userId = column[String]("USERNAME", O.NotNull, O.DBType("TEXT"))
+  def roleName = column[String]("ROLE_NAME", O.NotNull, O.DBType("TEXT"))
 
   def pk = primaryKey("USER_ROLES_PK", (userId, roleName))
   def user = foreignKey("USER_FK", userId, models.users)(_.userId, onDelete = ForeignKeyAction.Cascade)
@@ -140,9 +140,9 @@ class DocumentsModel(tag: Tag) extends Table[Document](tag, "DOCUMENTS") {
   import models._
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def serialId = column[Option[SerialNumber]]("SERIAL_ID")
-  def docType = column[String]("DOC_TYPE", O.NotNull)
-  def mailbox = column[String]("MAILBOX", O.NotNull)
-  def creator = column[String]("CREATOR", O.NotNull)
+  def docType = column[String]("DOC_TYPE", O.NotNull, O.DBType("TEXT"))
+  def mailbox = column[String]("MAILBOX", O.NotNull, O.DBType("TEXT"))
+  def creator = column[String]("CREATOR", O.NotNull, O.DBType("TEXT"))
   def created = column[DateTime]("CREATED", O.NotNull)
   def forTenant = column[Int]("FOR_TENANT", O.NotNull)
   def year = column[Int]("YEAR", O.NotNull)
@@ -151,7 +151,7 @@ class DocumentsModel(tag: Tag) extends Table[Document](tag, "DOCUMENTS") {
   def amountPaid = column[JsObject]("AMOUNT_PAID", O.NotNull)
   def body = column[JsObject]("BODY", O.NotNull)
   def comments = column[JsObject]("COMMENTS", O.NotNull)
-  def assigned = column[Option[String]]("ASSIGNED")
+  def assigned = column[Option[String]]("ASSIGNED", O.DBType("TEXT"))
 
   def lastAction = column[Option[Int]]("LAST_ACTION_ID")
   def preparedAction = column[Option[Int]]("PREPARED_ACTION_ID")
@@ -162,8 +162,8 @@ class DocumentsModel(tag: Tag) extends Table[Document](tag, "DOCUMENTS") {
 }
 
 class MailTokensModel(tag: Tag) extends Table[MailToken](tag, "MAIL_TOKENS") {
-  def uuid = column[String]("UUID", O.PrimaryKey)
-  def email = column[String]("EMAIL", O.NotNull)
+  def uuid = column[String]("UUID", O.PrimaryKey, O.DBType("TEXT"))
+  def email = column[String]("EMAIL", O.NotNull, O.DBType("TEXT"))
   def creationTime = column[DateTime]("CREATION_TIME", O.NotNull)
   def expirationTime = column[DateTime]("EXPIRATION_TIME", O.NotNull)
   def isSignUp = column[Boolean]("IS_SIGN_UP", O.NotNull)
@@ -173,7 +173,7 @@ class MailTokensModel(tag: Tag) extends Table[MailToken](tag, "MAIL_TOKENS") {
 }
 
 class InvitationsModel(tag: Tag) extends Table[InvitationInfo](tag, "INVITATIONS") {
-  def email = column[String]("EMAIL", O.PrimaryKey)
+  def email = column[String]("EMAIL", O.PrimaryKey, O.DBType("TEXT"))
   def isEncoder = column[Boolean]("IS_ENCODER", O.NotNull)
   def isChecker = column[Boolean]("IS_CHECKER", O.NotNull)
   def isApprover = column[Boolean]("IS_APPROVER", O.NotNull)
@@ -185,10 +185,10 @@ class InvitationsModel(tag: Tag) extends Table[InvitationInfo](tag, "INVITATIONS
 
 class ActionLogsModel(tag: Tag) extends Table[ActionLog](tag, "ACTION_LOGS") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-  def who = column[String]("WHO")
+  def who = column[String]("WHO", O.DBType("TEXT"))
   def what = column[Int]("WHAT", O.NotNull)
   def when = column[DateTime]("WHEN", O.NotNull)
-  def why = column[String]("WHY", O.NotNull)
+  def why = column[String]("WHY", O.NotNull, O.DBType("TEXT"))
 
   def _who = foreignKey("USER_FK", who, models.users)(_.userId, onDelete = ForeignKeyAction.SetNull)
   def _what = foreignKey("DOCUMENT_FK", what, models.documents)(_.id, onDelete = ForeignKeyAction.Cascade)
