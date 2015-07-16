e196206d698fc850809bfbc6bda65d7ed930bcd6
Akie Mejia <locker47@gmail.com>
2015-07-16 11:44:54 +0900

    Added monthly amounts

--- app/com/nooovle/Amounts.scala
+++ app/com/nooovle/Amounts.scala
@@ -2,11 +2,7 @@ package com.nooovle
 
 import play.api.libs.json._
 
-case class Amounts(
-    total: Double,
-    paid: Double
-) {
-
+case class Amounts(total: Double, paid: Double) {
   val unpaid: Double = total - paid
   val isPaid: Boolean = unpaid == 0
 
@@ -30,4 +26,8 @@ case class Amounts(
 
 object Amounts {
   val Zero = Amounts(0.0, 0.0)
+}
+
+case class MonthlyAmounts(rent: Amounts, electricity: Amounts, water: Amounts, cusa: Amounts) {
+  val isPaid = List(rent, electricity, water, cusa).foldLeft(true)(_ && _.isPaid)
 }
